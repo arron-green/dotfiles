678c40c91b862b2d11033f625219655cab8d29e0
Arron Green <arron.green@reaktor.com>
2017-05-02 17:17:37 +0300

    cleanup and global gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,14 @@
 VUNDLE_HOME=~/.vim/bundle/Vundle.vim
 DEV_HOME=~/dev
+REPO_PATH=$(git rev-parse --show-toplevel)
 
 function exists-in-path {
-    ARG=$1
-    if [[ ! -z $ARG ]]; then
-        type -p $ARG > /dev/null 2>&1
+    if [[ ! -z $1 ]]; then
+        type -p $1 > /dev/null 2>&1
         return $?
+    else
+        return 1
     fi
-    return 1
 }
 
 [ -d $DEV_HOME ] || mkdir -p $DEV_HOME
@@ -17,9 +18,8 @@ exists-in-path brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercont
 cd brew && brew bundle
 
 # link vimrc
-if [[ ! -L ~/.vimrc ]]; then
-  ln -s $PWD/vimrc ~/.vimrc
-fi
+[[ -L ~/.vimrc && "$REPO_PATH/vimrc" == "$(readlink ~/.vimrc)" ]] || ([[ -e ~/.vimrc ]] && rm ~/.vimrc)
+[[ -L ~/.vimrc ]] || ln -s $REPO_PATH/vimrc ~/.vimrc
 
 # download vundle
 if [[ ! -e $VUNDLE_HOME ]]; then
@@ -32,22 +32,21 @@ vim +PluginInstall +qall
 # setup python environment
 exists-in-path pip && exists-in-path virtualenvwrapper.sh || pip install virtualenvwrapper
 
-# link profile
-if [[ ! -L ~/.profile ]]; then
-  ln -s $PWD/profile.sh ~/.profile
-fi
-
 # create local .bin dir
 [ -d ~/.bin ] || mkdir ~/.bin
 
 # create virtualenvs directory
 [ -d ~/.virtualenvs ] || mkdir ~/.virtualenvs
 
-# link git info
-if [[ ! -L ~/.gitconfig ]]; then
-    # rm if exists but is not a symlink
-    [[ -e ~/.gitconfig ]] && rm ~/.gitconfig
-    # link to dotfiles gitconfig
-    ln -s $PWD/gitconfig ~/.gitconfig
-fi
+# link bash profile
+[[ -L ~/.profile && "$REPO_PATH/profile.sh" == "$(readlink ~/.profile)" ]] || ([[ -e ~/.profile ]] && rm ~/.profile)
+[[ -L ~/.profile ]] || ln -s $REPO_PATH/profile.sh ~/.profile
+
+# link gitconfig
+[[ -L ~/.gitconfig && "$REPO_PATH/gitconfig" == "$(readlink ~/.gitconfig)" ]] || ([[ -e ~/.gitconfig ]] && rm ~/.gitconfig)
+[[ -L ~/.gitconfig ]] || ln -s $REPO_PATH/gitconfig ~/.gitconfig
+
+# link global gitignore
+[[ -L ~/.gitignore && "$REPO_PATH/gitignore" == "$(readlink ~/.gitignore)" ]] || ([[ -e ~/.gitignore ]] && rm ~/.gitignore)
+[[ -L ~/.gitignore ]] || ln -s $REPO_PATH/gitignore ~/.gitignore
 
