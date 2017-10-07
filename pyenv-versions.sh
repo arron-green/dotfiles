6ac529bcba37d2968be3d6735cf4f9e6584261ee
Arron Green <arrong@gmail.com>
2017-10-07 11:54:06 -0400

    remove extension

--- pyenv-versions.sh
+++
@@ -1,3 +0,0 @@
-for python_version in 2.7.10 2.7.13 3.3.6 3.5.3 3.6.2; do
-    pyenv install -s $python_version
-done
--- rbenv-versions.sh
+++
@@ -1,3 +0,0 @@
-for RUBY_VERSION in 2.1.10 2.2.7 2.3.4 2.4.1; do
-    rbenv install -v -s $RUBY_VERSION
-done
