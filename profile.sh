2f9f311745513cdb120c7e1e7a078612f51e519c
Arron Green <arrong@gmail.com>
2018-07-24 18:18:46 -0400

    fix TERM colors

--- profile.sh
+++ profile.sh
@@ -1,6 +1,8 @@
 # adds color to my terminal
-export TERM=xterm-color
-export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
+export TERM=xterm-256color
+export COLORTERM="truecolor"
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 case $TERM in
